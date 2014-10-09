ba7bb66ba3941ebc632b928a0da5068c7588fb3f
sintjuri <sintjuri@yandex.ru>
2014-10-09 09:22:25 +0400

    simple compass

--- app/src/main/java/com/onettm/directions/MainActivity.java
+++ app/src/main/java/com/onettm/directions/MainActivity.java
@@ -4,11 +4,13 @@ package com.onettm.directions;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
+import android.graphics.Matrix;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
@@ -20,7 +22,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     private ImageView image;
 
     // record the compass picture angle turned
-    private float currentDegree = 0f;
+    private double currentDegree = 0f;
 
     private TextView tvHeading;
 
@@ -37,7 +39,6 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //
         image = (ImageView) findViewById(R.id.imageViewCompass);
 
         // TextView that will tell the user what degree is he heading
@@ -64,8 +65,9 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onResume();
 
         // for the system's orientation sensor registered listeners
-        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
+
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_FASTEST);
 
 
     }
@@ -113,107 +115,16 @@ public class MainActivity extends Activity implements SensorEventListener {
             }
         }
 
-        // Do something with these orientation angles.
 
+        tvHeading.setText("Heading: " + Double.toString(Math.toDegrees(azimut)) + " degrees");
 
-        // get the angle around the z-axis rotated
-        //float degree = Math.round(azimut);
-
-        tvHeading.setText("Heading: " + Float.toString(-azimut*360/(2*3.14159f)) + " degrees");
-
-        // create a rotation animation (reverse turn degree degrees)
-        RotateAnimation ra = new RotateAnimation(
-                currentDegree,
-                -azimut*360/(2*3.14159f),
-                Animation.RELATIVE_TO_SELF, 0.5f,
-                Animation.RELATIVE_TO_SELF,
-                0.5f);
-
-        // how long the animation will take place
-        ra.setDuration(210);
-
-        // set the animation after the end of the reservation status
-        ra.setFillAfter(true);
-
-        // Start the animation
-        image.startAnimation(ra);
-        currentDegree = -azimut*360/(2*3.14159f);
+        Matrix matrix=new Matrix();
+        image.setScaleType(ImageView.ScaleType.MATRIX);   //required
+        matrix.postRotate((float) (-1*Math.toDegrees(azimut)), image.getDrawable().getBounds().width()/2, image.getDrawable().getBounds().height()/2);
+        image.setImageMatrix(matrix);
+        currentDegree = Math.toDegrees(azimut);
 
     }
 
 
 }
-/*
-
-package com.onettm.directions;
-
-import java.util.List;
-
-import android.app.Activity;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
-
-public class MainActivity extends Activity {
-
-    TextView tvText;
-    SensorManager sensorManager;
-    List<Sensor> sensors;
-    Sensor sensorLight;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tvText = (TextView) findViewById(R.id.tvText);
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensors = sensorManager.getSensorList(Sensor.TYPE_ALL);
-        sensorLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-    }
-
-    public void onClickSensList(View v) {
-
-        sensorManager.unregisterListener(listenerLight, sensorLight);
-        StringBuilder sb = new StringBuilder();
-
-        for (Sensor sensor : sensors) {
-            sb.append("name = ").append(sensor.getName())
-                    .append(", type = ").append(sensor.getType())
-                    .append("\nvendor = ").append(sensor.getVendor())
-                    .append(" ,version = ").append(sensor.getVersion())
-                    .append("\nmax = ").append(sensor.getMaximumRange())
-                    .append(", resolution = ").append(sensor.getResolution())
-                    .append("\n--------------------------------------\n");
-        }
-        tvText.setText(sb);
-    }
-
-    public void onClickSensLight(View v) {
-        sensorManager.registerListener(listenerLight, sensorLight,
-                SensorManager.SENSOR_DELAY_NORMAL);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        sensorManager.unregisterListener(listenerLight, sensorLight);
-    }
-
-    SensorEventListener listenerLight = new SensorEventListener() {
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
-
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            tvText.setText(String.valueOf(event.values[0]));
-        }
-    };
-
-}
-*/
