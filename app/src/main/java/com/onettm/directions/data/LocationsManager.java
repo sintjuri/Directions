69df433a55921674f4068da97a886e5f648439d9
Alexey Grigoryev <alexey.grigoryev@t-systems.ru>
2015-01-20 11:54:26 +0300

    implement cancellation of search task

--- app/src/main/java/com/onettm/directions/data/LocationsManager.java
+++ app/src/main/java/com/onettm/directions/data/LocationsManager.java
@@ -16,6 +16,7 @@ import java.util.HashSet;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.Set;
+import java.util.concurrent.CancellationException;
 
 /**
  * Created by agrigory on 1/16/15.
@@ -23,9 +24,8 @@ import java.util.Set;
  */
 public class LocationsManager extends Observable implements Observer {
 
-    private Collection<LocationItem> locations = Collections.unmodifiableCollection(Collections.<LocationItem>emptyList());
     private final com.onettm.directions.DirectionsApplication.Settings settings = DirectionsApplication.getInstance().getSettings();
-
+    private Collection<LocationItem> locations = Collections.unmodifiableCollection(Collections.<LocationItem>emptyList());
     private volatile boolean valid = true;
     private Location lastLocation;
     private Location decisionLocation;
@@ -36,10 +36,19 @@ public class LocationsManager extends Observable implements Observer {
     }
 
 
-    public synchronized boolean isRunning() {
-        if (searchTask != null)
-            if (searchTask.getStatus() != AsyncTask.Status.FINISHED) return true;
+    public boolean isRunning() {
+        AsyncTask t = searchTask;
+        if (t != null)
+            if (t.isCancelled()) return false;
+        if (t.getStatus() != AsyncTask.Status.FINISHED) return true;
         return false;
+    }
+
+    public void cancel() {
+        AsyncTask t = searchTask;
+        if (t == null) return;
+        if (t.isCancelled()) return;
+        if (t.getStatus() != AsyncTask.Status.FINISHED) searchTask.cancel(true);
     }
 
     private void setValid() {
@@ -70,7 +79,16 @@ public class LocationsManager extends Observable implements Observer {
 
             @Override
             protected Void doInBackground(Location[] params) {
-                request(params[0], this);
+                Set<LocationItem> result = request(params[0]);
+                if (!locations.containsAll(result)) {
+                    ArrayList<LocationItem> res = new ArrayList<LocationItem>(result);
+                    DistanceComparator<LocationItem> dc = new DistanceComparator<LocationItem>(params[0]);
+                    Collections.sort(res, dc);
+
+                    locations = Collections.unmodifiableCollection(res);
+                    decisionLocation = params[0];
+                    setChanged();
+                }
                 return null;
             }
 
@@ -89,90 +107,82 @@ public class LocationsManager extends Observable implements Observer {
                 setChanged();
                 notifyObservers();
             }
+
+            private Set<LocationItem> request(Location location) {
+
+                DistanceComparator<LocationItem> dc = new DistanceComparator<LocationItem>(location);
+
+                long longitudeSide = Math.round(getLongitudeDegreesByDistance(location, DirectionsApplication.getInstance().getSettings().getSearchRadius()) * 10000000);
+                long latitudeSide = Math.round(getLatitudeDegreesByDistance(location, DirectionsApplication.getInstance().getSettings().getSearchRadius()) * 10000000);
+
+                long cur_lat = (long) (location.getLatitude() * 10000000);
+                long cur_lon = (long) (location.getLongitude() * 10000000);
+                Cursor c = DirectionsApplication.getInstance().getDb().rawQuery(String.format("select tag.v, node.lat, node.lon from node \n" +
+                        "join tag_node on node.id=tag_node.node\n" +
+                        "join tag on tag.id=tag_node.tag\n" +
+                        "where tag.k='name'\n" +
+                        "and node.lat > %d and node.lat < %d \n" +
+                        "and node.lon > %d and node.lon < %d ", cur_lat - latitudeSide, cur_lat + latitudeSide, cur_lon - longitudeSide, cur_lon + longitudeSide), null);
+
+                c.moveToFirst();
+                Set<LocationItem> result = new HashSet<LocationItem>();
+                while (c.moveToNext()) {
+                    if (isCancelled()) throw new CancellationException();
+                    String name = c.getString(0);
+                    double lat = c.getDouble(1) / 10000000;
+                    double lon = c.getDouble(2) / 10000000;
+                    Location pv = new Location("");
+                    pv.setLatitude(lat);
+                    pv.setLongitude(lon);
+                    result.add(new LocationItem(pv, name, location));
+                }
+                c.close();
+
+                Cursor c2 = DirectionsApplication.getInstance().getDb().rawQuery(String.format("select tag.v, node.lat, node.lon, way.id from node \n" +
+                        "join way on way.id = node.way\n" +
+                        "join tag_way on way.id = tag_way.way\n" +
+                        "join tag on tag.id=tag_way.tag\n" +
+                        "where tag.k='name'\n" +
+                        "and node.lat > %d and node.lat < %d \n" +
+                        "and node.lon > %d and node.lon < %d ", cur_lat - latitudeSide, cur_lat + latitudeSide, cur_lon - longitudeSide, cur_lon + longitudeSide), null);
+
+                c2.moveToFirst();
+                SparseArray<LocationItem> wayNodes = new SparseArray<LocationItem>();
+
+                while (c2.moveToNext()) {
+                    if (isCancelled()) throw new CancellationException();
+                    String name = c2.getString(0);
+                    double lat = c2.getDouble(1) / 10000000;
+                    double lon = c2.getDouble(2) / 10000000;
+                    Integer way = c2.getInt(3);
+                    Location pv = new Location("");
+                    pv.setLatitude(lat);
+                    pv.setLongitude(lon);
+                    LocationItem newItem = new LocationItem(pv, name, location);
+                    LocationItem existingItem = wayNodes.get(way);
+                    if (existingItem != null) {
+                        if (dc.compare(existingItem, newItem) < 0) continue;
+                    }
+                    wayNodes.put(c2.getInt(3), newItem);
+                }
+                c2.close();
+
+                int key;
+                for (int i = 0; i < wayNodes.size(); i++) {
+                    if (isCancelled()) throw new CancellationException();
+                    key = wayNodes.keyAt(i);
+                    LocationItem obj = wayNodes.get(key);
+                    result.add(obj);
+                }
+
+                return result;
+            }
         };
         searchTask = at.execute(curLoc);
     }
 
     public Collection<LocationItem> getLocationItems() {
         return locations;
-    }
-
-    private synchronized void request(Location location, AsyncTask<Location, Void, Void> asyncTask) {
-
-        DistanceComparator<LocationItem> dc = new DistanceComparator<LocationItem>(location);
-
-        long longitudeSide = Math.round(getLongitudeDegreesByDistance(location, DirectionsApplication.getInstance().getSettings().getSearchRadius()) * 10000000);
-        long latitudeSide = Math.round(getLatitudeDegreesByDistance(location, DirectionsApplication.getInstance().getSettings().getSearchRadius()) * 10000000);
-
-        long cur_lat = (long) (location.getLatitude() * 10000000);
-        long cur_lon = (long) (location.getLongitude() * 10000000);
-        Cursor c = DirectionsApplication.getInstance().getDb().rawQuery(String.format("select tag.v, node.lat, node.lon from node \n" +
-                "join tag_node on node.id=tag_node.node\n" +
-                "join tag on tag.id=tag_node.tag\n" +
-                "where tag.k='name'\n" +
-                "and node.lat > %d and node.lat < %d \n" +
-                "and node.lon > %d and node.lon < %d ", cur_lat - latitudeSide, cur_lat + latitudeSide, cur_lon - longitudeSide, cur_lon + longitudeSide), null);
-
-        c.moveToFirst();
-        Set<LocationItem> result = new HashSet<LocationItem>();
-        while (c.moveToNext()) {
-            if (asyncTask.isCancelled()) return;
-            String name = c.getString(0);
-            double lat = c.getDouble(1) / 10000000;
-            double lon = c.getDouble(2) / 10000000;
-            Location pv = new Location("");
-            pv.setLatitude(lat);
-            pv.setLongitude(lon);
-            result.add(new LocationItem(pv, name, location));
-        }
-        c.close();
-
-        Cursor c2 = DirectionsApplication.getInstance().getDb().rawQuery(String.format("select tag.v, node.lat, node.lon, way.id from node \n" +
-                "join way on way.id = node.way\n" +
-                "join tag_way on way.id = tag_way.way\n" +
-                "join tag on tag.id=tag_way.tag\n" +
-                "where tag.k='name'\n" +
-                "and node.lat > %d and node.lat < %d \n" +
-                "and node.lon > %d and node.lon < %d ", cur_lat - latitudeSide, cur_lat + latitudeSide, cur_lon - longitudeSide, cur_lon + longitudeSide), null);
-
-        c2.moveToFirst();
-        SparseArray<LocationItem> wayNodes = new SparseArray<LocationItem>();
-
-        while (c2.moveToNext()) {
-            if (asyncTask.isCancelled()) return;
-            String name = c2.getString(0);
-            double lat = c2.getDouble(1) / 10000000;
-            double lon = c2.getDouble(2) / 10000000;
-            Integer way = c2.getInt(3);
-            Location pv = new Location("");
-            pv.setLatitude(lat);
-            pv.setLongitude(lon);
-            LocationItem newItem = new LocationItem(pv, name, location);
-            LocationItem existingItem = wayNodes.get(way);
-            if (existingItem != null) {
-                if (dc.compare(existingItem, newItem) < 0) continue;
-            }
-            wayNodes.put(c2.getInt(3), newItem);
-        }
-        c2.close();
-
-        int key;
-        for(int i = 0; i < wayNodes.size(); i++) {
-            if (asyncTask.isCancelled()) return;
-            key = wayNodes.keyAt(i);
-            LocationItem obj = wayNodes.get(key);
-            result.add(obj);
-        }
-
-        if (!this.locations.containsAll(result)) {
-            if (asyncTask.isCancelled()) return;
-            ArrayList<LocationItem> res = new ArrayList<LocationItem>(result);
-            Collections.sort(res, dc);
-
-            this.locations = Collections.unmodifiableCollection(res);
-            decisionLocation = location;
-            this.setChanged();
-        }
     }
 
     public boolean isValid() {
@@ -189,30 +199,6 @@ public class LocationsManager extends Observable implements Observer {
                 if (lastLocation.distanceTo(decisionLocation) < settings.getDecisionExpirationDistance())
                     return;
         update(lastLocation);
-    }
-
-
-    static class DistanceComparator<L extends LocationItem> implements java.util.Comparator<L> {
-
-        private final Location currentLoc;
-
-        DistanceComparator(Location currentLoc) {
-            this.currentLoc = currentLoc;
-        }
-
-        @Override
-        public int compare(L l, L l2) {
-            if (l == l2) return 0;
-            if (l == null) return -1;
-            if (l2 == null) return 1;
-
-            if (l.getLocation() == null) return -1;
-            if (l2.getLocation() == null) return 1;
-
-            float d = currentLoc.distanceTo(l.getLocation());
-            float d2 = currentLoc.distanceTo(l2.getLocation());
-            return Float.compare(d, d2);
-        }
     }
 
     private float getLatitudeDegreesByDistance(Location location, int meters) {
@@ -235,5 +221,28 @@ public class LocationsManager extends Observable implements Observer {
         hiLoc.setLongitude(hi);
         float d = loLoc.distanceTo(hiLoc);
         return meters * (1 / d);
+    }
+
+    static class DistanceComparator<L extends LocationItem> implements java.util.Comparator<L> {
+
+        private final Location currentLoc;
+
+        DistanceComparator(Location currentLoc) {
+            this.currentLoc = currentLoc;
+        }
+
+        @Override
+        public int compare(L l, L l2) {
+            if (l == l2) return 0;
+            if (l == null) return -1;
+            if (l2 == null) return 1;
+
+            if (l.getLocation() == null) return -1;
+            if (l2.getLocation() == null) return 1;
+
+            float d = currentLoc.distanceTo(l.getLocation());
+            float d2 = currentLoc.distanceTo(l2.getLocation());
+            return Float.compare(d, d2);
+        }
     }
 }
