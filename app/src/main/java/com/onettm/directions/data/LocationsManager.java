4562c32eb56419e59674d92f713521ddfa0a204c
Alexey Grigoryev <alexey.grigoryev@t-systems.ru>
2015-01-20 10:04:23 +0300

    use radius for searching

--- app/src/main/java/com/onettm/directions/data/LocationsManager.java
+++ app/src/main/java/com/onettm/directions/data/LocationsManager.java
@@ -8,11 +8,9 @@ import com.onettm.directions.DirectionsApplication;
 import com.onettm.directions.LocationItem;
 import com.onettm.directions.Model;
 
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -50,7 +48,7 @@ public class LocationsManager extends Observable implements Observer {
         this.valid = false;
     }
 
-    public synchronized void invalidate(){
+    public synchronized void invalidate() {
         setInvalid();
         update(lastLocation);
     }
@@ -60,7 +58,7 @@ public class LocationsManager extends Observable implements Observer {
             if (running) return;
         setInvalid();
 
-        AsyncTask<Location, Void, Void> at = new AsyncTask<Location, Void, Void>(){
+        AsyncTask<Location, Void, Void> at = new AsyncTask<Location, Void, Void>() {
             @Override
             protected void onPreExecute() {
                 super.onPreExecute();
@@ -88,25 +86,26 @@ public class LocationsManager extends Observable implements Observer {
         AsyncTask res = at.execute(curLoc);
     }
 
-    public Collection<LocationItem> getLocationItems(){
+    public Collection<LocationItem> getLocationItems() {
         return locations;
     }
 
-    private synchronized void request(Location location){
+    private synchronized void request(Location location) {
 
         Location currentLocation = location;
         DistanceComparator<LocationItem> dc = new DistanceComparator<LocationItem>(currentLocation);
 
+        float longitudeSide = getLongitudeDegreesByDistance(currentLocation, DirectionsApplication.getInstance().getSettings().getSearchRadius()) * 10000000;
+        float latitudeSide = getLatitudeDegreesByDistance(currentLocation, DirectionsApplication.getInstance().getSettings().getSearchRadius()) * 10000000;
 
-        System.err.println("TIME getDestinations 3 " + new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()));
         long cur_lat = (long) (currentLocation.getLatitude() * 10000000);
         long cur_lon = (long) (currentLocation.getLongitude() * 10000000);
         Cursor c = DirectionsApplication.getInstance().getDb().rawQuery(String.format("select tag.v, node.lat, node.lon from node \n" +
                 "join tag_node on node.id=tag_node.node\n" +
                 "join tag on tag.id=tag_node.tag\n" +
                 "where tag.k='name'\n" +
-                "and node.lat > %d - 400000 and node.lat < %d + 400000\n" +
-                "and node.lon > %d - 700000 and node.lon < %d + 700000", cur_lat, cur_lat, cur_lon, cur_lon), null);
+                "and node.lat > %d and node.lat < %d \n" +
+                "and node.lon > %d and node.lon < %d ", cur_lat - latitudeSide, cur_lat + latitudeSide, cur_lon - longitudeSide, cur_lon + longitudeSide), null);
 
         c.moveToFirst();
         Set<LocationItem> result = new HashSet<LocationItem>();
@@ -119,17 +118,15 @@ public class LocationsManager extends Observable implements Observer {
             pv.setLongitude(lon);
             result.add(new LocationItem(pv, name, currentLocation));
         }
-        System.err.println("TIME getDestinations 8 " + new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()));
         c.close();
-        System.err.println("TIME getDestinations 9 " + new SimpleDateFormat("HH:mm:ss.SSS").format(new Date()));
 
         Cursor c2 = DirectionsApplication.getInstance().getDb().rawQuery(String.format("select tag.v, node.lat, node.lon, way.id from node \n" +
                 "join way on way.id = node.way\n" +
                 "join tag_way on way.id = tag_way.way\n" +
                 "join tag on tag.id=tag_way.tag\n" +
                 "where tag.k='name'\n" +
-                "and node.lat > %d - 400000 and node.lat < %d + 400000\n" +
-                "and node.lon > %d - 700000 and node.lon < %d + 700000", cur_lat, cur_lat, cur_lon, cur_lon), null);
+                "and node.lat > %d and node.lat < %d \n" +
+                "and node.lon > %d and node.lon < %d ", cur_lat - latitudeSide, cur_lat + latitudeSide, cur_lon - longitudeSide, cur_lon + longitudeSide), null);
 
         c2.moveToFirst();
         Map<Integer, LocationItem> wayNodes = new HashMap<Integer, LocationItem>();
@@ -153,7 +150,7 @@ public class LocationsManager extends Observable implements Observer {
         result.addAll(wayNodes.values());
 
         if (!this.locations.containsAll(result)) {
-            ArrayList<LocationItem> res= new ArrayList<LocationItem>(result);
+            ArrayList<LocationItem> res = new ArrayList<LocationItem>(result);
             Collections.sort(res, dc);
 
             this.locations = Collections.unmodifiableCollection(res);
@@ -170,10 +167,11 @@ public class LocationsManager extends Observable implements Observer {
     public void update(Observable observable, Object data) {
         if (!(data instanceof Location)) throw new AssertionError("Location object expected");
         lastLocation = (Location) data;
-        if ( running ) return;
-        if(isValid())
-            if(decisionLocation != null)
-                if (lastLocation.distanceTo(decisionLocation) < settings.getDecisionExpirationDistance()) return;
+        if (running) return;
+        if (isValid())
+            if (decisionLocation != null)
+                if (lastLocation.distanceTo(decisionLocation) < settings.getDecisionExpirationDistance())
+                    return;
         update(lastLocation);
     }
 
@@ -201,7 +199,7 @@ public class LocationsManager extends Observable implements Observer {
         }
     }
 
-    private float getLatitudeDegreesByDistance(Location location, int meters){
+    private float getLatitudeDegreesByDistance(Location location, int meters) {
         double lo = Math.floor(location.getLatitude());
         double hi = Math.ceil(location.getLatitude());
         Location loLoc = new Location("dummy");
@@ -209,11 +207,11 @@ public class LocationsManager extends Observable implements Observer {
         Location hiLoc = new Location("dummy");
         hiLoc.setLatitude(hi);
         float d = loLoc.distanceTo(hiLoc);
-        float k = meters*(1/d);
+        float k = meters * (1 / d);
         return k;
     }
 
-    private float getLongitudeDegreesByDistance(Location location, int meters){
+    private float getLongitudeDegreesByDistance(Location location, int meters) {
         double lo = Math.floor(location.getLongitude());
         double hi = Math.ceil(location.getLongitude());
         Location loLoc = new Location("dummy");
@@ -221,7 +219,7 @@ public class LocationsManager extends Observable implements Observer {
         Location hiLoc = new Location("dummy");
         hiLoc.setLongitude(hi);
         float d = loLoc.distanceTo(hiLoc);
-        float k = meters*(1/d);
+        float k = meters * (1 / d);
         return k;
     }
 }
